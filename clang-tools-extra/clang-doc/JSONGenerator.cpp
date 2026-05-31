3a1bc39272ade6962ae497644afb74656561705d
Paul Kirth <pk1574@gmail.com>
2026-05-31 09:18:22 -0700

    [clang-doc] Use modern LLVM style license headers
    See https://llvm.org/docs/CodingStandards.html#file-headers

--- clang-tools-extra/clang-doc/JSONGenerator.cpp
+++ clang-tools-extra/clang-doc/JSONGenerator.cpp
@@ -1,4 +1,4 @@
-//===-- JSONGenerator.cpp - JSON Generator ----------------------*- C++ -*-===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
